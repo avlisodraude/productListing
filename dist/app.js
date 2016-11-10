73ef01ec75b2186aa3dd0e0ea00fd594a0c5f6ed
E.Y. Silva Navarrete <e.y.silvanavarrete@europeana.eu>
2016-11-10 15:38:32 +0100

    products display and search filter

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,73 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var axios = require('axios');
+var _ = require('underscore');
 
-class App extends React.Component {
-    render() {
-        return <div>
-        <h1>This is my AWESOME App</h1>
-        <p>This is Eves app created with React, webpack and magic!</p>
-        </div>
+require('./scss/main.scss');
+
+
+var App = React.createClass({
+
+    filterItems: function(event){
+        var updatedList = this.state.initialItems;
+        updatedList = updatedList.filter(function(item){
+            return item.toLowerCase().search(
+                    event.target.value.toLowerCase()) !== -1;
+        });
+        this.setState({items: updatedList});
+    },
+
+    getInitialState: function(){
+        return {
+            initialItems: [],
+            items: []
+        }
+    },
+
+    componentWillMount: function(){
+        var th = this;
+        var arr = [];
+        this.serverRequest =
+            axios.get(this.props.source)
+                .then(function (result) {
+
+                    _(result.data.worksById).each(function(elem, key){
+                        arr.push(elem.Title.TitleText);
+                    });
+
+                    th.setState({
+                        initialItems: arr
+                    });
+                });
+        this.setState({items: arr})
+    },
+
+    componentWillUnmount: function () {
+        this.serverRequest.abort();
+    },
+
+    render: function(){
+        return (
+            <div className="listContainer">
+                <input type="text" placeholder="Search" onChange={this.filterItems}/>
+                <List items={this.state.items}/>
+            </div>
+        )
     }
-}
+});
 
-ReactDOM.render(<App />, document.getElementById('react-container'));
+var List = React.createClass({
+    render: function(){
+        return (
+            <ul>
+            {
+                this.props.items.map(function(item) {
+                return <li key={item}>{item}</li>
+            })
+            }
+        </ul>
+        )
+    }
+});
+
+ReactDOM.render(<App source="./products.json" />, document.querySelector("#wrapper"));
